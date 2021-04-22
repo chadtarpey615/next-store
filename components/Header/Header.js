53a2e9cd9825b66df9a65bb1fff465b841a01a3c
Chad Tarpey <chadtarpey@gmail.com>
2021-04-22 13:33:47 -0500

    created navbar.js and header.js

---
+++ components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div className="header">
+            <h1>Welcome to Chad's Store</h1>
+        </div>
+    )
+}
+
+export default Header;
